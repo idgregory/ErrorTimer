003253767e9847f10bae543a1160ee9207c090a4
idgregory <60363703+idgregory@users.noreply.github.com>
2020-02-20 11:51:01 -0600

    Add files via upload

---
+++ ErrorTimer/ErrorTimer.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Timer
+{
+    class ErrorTimer
+    {
+        private bool isError, hasChange;
+        private Thread t;
+
+        public ErrorTimer()
+        {
+            isError = false;
+            hasChange = false;
+            t = new Thread(() => { hasError = waitForError(); });
+        }
+
+        //Starts the thread that waits a certain amount of time
+        public void start()
+        {
+            t.Start();
+        }
+
+        //Checks if thread is still running
+        public bool isAlive()
+        {
+            return t.IsAlive;
+        }
+
+        //get set method that invokes a function when the variable is changed
+        public bool hasError
+        {
+            get { return isError; }
+
+            set
+            {
+                isError = value;
+                OnErrorChange();
+            }
+        }
+
+        //Method invoded by hasError
+        private void OnErrorChange()
+        {
+
+            if(!this.hasChange)
+            {
+                Console.WriteLine("Continue");
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+        }
+
+        //Waits three seconds then executes code
+        public bool waitForError()
+        {
+            Random r = new Random();
+            int rand = r.Next(0, 1);
+            Thread.Sleep(3000);
+            return (rand == 1); ;
+        }
+
+    }
+}
